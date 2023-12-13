d92225bf00f6096b14b58ab285d2f9f292f5a81a
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2023-12-13 17:19:34 +0100

    [Example_3] Alghorithms fro LeetCode

--- example_3/src/main.rs
+++ example_3/src/main.rs
@@ -1,3 +1,5 @@
+use indexmap::IndexMap;
+
 fn maskify(mut string: String) -> String {
     let len = string.len();
 
@@ -12,9 +14,73 @@ fn maskify(mut string: String) -> String {
     return string;
 }
 
+fn two_sums(array: Vec<u32>, target: u32) -> [u8; 2] {
+    for (i, _el) in array.iter().enumerate() {
+        if  array[i] + array[i + 1] == target {
+            return [i.try_into().unwrap(), (i + 1).try_into().unwrap()];
+        }
+    }
+
+    return [0, 0];
+}
+
+fn is_palindrome(first_number: u32) -> bool {
+    let str = first_number.to_string();
+    return str.chars().rev().collect::<String>() == str
+}
+
+fn integer_to_roman(mut number: u32) -> String {
+    let mut roman_number: String = "".to_string();
+    let roman_table: IndexMap<&str, u32> = IndexMap::from([
+        ("M", 1000),
+        ("D", 500),
+        ("C", 100),
+        ("L", 50),
+        ("X", 10),
+        ("V", 5),
+        ("I", 1),
+    ])
+    .into_iter()
+    .collect();
+
+    while number > 0 {
+        for (key, value) in roman_table.iter() {
+            if number >= *value {
+                number -= value;
+                roman_number += key;
+                break;
+            };
+        }
+    }
+
+    return roman_number.to_string();
+}
+
+fn longest_common_prefix(mut array: Vec<&str>) -> String {
+    array.sort();
+
+    let first_string = array[0];
+    let last_string = array[array.len() - 1];
+    let mut idx = 0;
+
+    while idx < first_string.len() && idx < last_string.len() {
+        if first_string.chars().nth(idx) == last_string.chars().nth(idx) {
+            idx += 1;
+        } else {
+            break;
+        }
+    }
+    
+    return first_string[..idx].to_string();
+}
+
 
 fn main() {
     println!("Code wars coding");
-    let original_string = String::from("Nananananananananananananananana Batman");
-    println!("Maskify result: {:?}", maskify(original_string));
+    println!("Maskify result: {:?}", maskify(String::from("3221-4432-1231-5412")));
+    println!("Two sums result: {:?}", two_sums([15, 24, 12, 43].to_vec(), 36));
+
+    println!("Is palindrome result: {:?}", is_palindrome(121));
+    println!("Is Integer to roman result: {:?}", integer_to_roman(142));
+    println!("Longest prefix result: {:?}", longest_common_prefix(["flower", "flow", "flight"].to_vec()));
 }
